c7f7db7cb0f1111c33d8dfabca529c363f292c5f
AJMV <marmolejandro@gmail.com>
2023-09-08 00:51:31 -0500

    Se eliminan DTOs mal nombrados

--- PruebaIngresoBibliotecario/PruebaIngresoBibliotecario.Api/Models/DtoPostOutLoan.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PruebaIngresoBibliotecario.Api.Models
-{
-    public class DtoPostOutLoan
-    {
-        public int Id { get; set; }
-        public DateTime FechaMaximaDevolucion { get; set;}
-    }
-}
