bf2e59455d7ac3bc879f2b5657777b6fd5981f21
AJMV <marmolejandro@gmail.com>
2023-09-08 00:52:28 -0500

    Se avanza con el controlador del proyecto  API

--- PruebaIngresoBibliotecario/PruebaIngresoBibliotecario.Api/Controllers/PrestamoController.cs
+++ PruebaIngresoBibliotecario/PruebaIngresoBibliotecario.Api/Controllers/PrestamoController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PruebaIngresoBibliotecario.Api.Models;
+using PruebaIngresoBibliotecario.Domain.Entities;
+using PruebaIngresoBibliotecario.Domain.Interfaces;
 
 namespace PruebaIngresoBibliotecario.Api.Controllers
 {
@@ -7,6 +10,29 @@ namespace PruebaIngresoBibliotecario.Api.Controllers
     [ApiController]
     public class PrestamoController : ControllerBase
     {
+        public IServicesLoan Services;
+
+        public PrestamoController(IServicesLoan _Services)
+        {
+            Services = _Services;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateLoan(CreateLoanInDto Loan)
+        {
+            List<CreateLoanOutDto >LoanOut = new List<CreateLoanOutDto>();
+
+            return Ok(LoanOut);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> SearchLoan(string Id)
+        {
+            List<Loan> LoanOut = new List<Loan>() ;
+            
+
+            return Ok(LoanOut);
+        }
 
     }
 }
