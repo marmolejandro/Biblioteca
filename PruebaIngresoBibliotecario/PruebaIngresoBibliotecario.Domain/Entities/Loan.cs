f5fee1d0603fd28f60a2ada7fbbba0a04f93acb0
AJMV <marmolejandro@gmail.com>
2023-09-08 00:53:04 -0500

    Se crea interfaz de servicios y Entidad para definir un prestamo

---
+++ PruebaIngresoBibliotecario/PruebaIngresoBibliotecario.Domain/Entities/Loan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaIngresoBibliotecario.Domain.Entities
+{
+    public class Loan
+    {
+        public string Id { get; set; }
+
+        public UserType TipoUsuario { get; set; }
+
+        public string IdentificacionUsuario { get; set; }
+
+        public Guid Isbn { get; set; }
+
+        public DateTime FechaMaximaDevolucion { get; set; }
+    }
+}
---
+++ PruebaIngresoBibliotecario/PruebaIngresoBibliotecario.Domain/Interfaces/IServicesLoan.cs
@@ -0,0 +1,16 @@
+﻿using PruebaIngresoBibliotecario.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaIngresoBibliotecario.Domain.Interfaces
+{
+    public interface IServicesLoan
+    {
+        public Task<bool> ValidateLoan(Loan _Loan);
+        public Loan SearchLoan(string Id);
+        public Loan CreateLoan(Loan _Loan);
+    }
+}
